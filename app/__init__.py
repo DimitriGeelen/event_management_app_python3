44cd518ea8fd90c9a6888dd1f21b0c7bc22aeb5f
DimitriGeelen <40521958+DimitriGeelen@users.noreply.github.com>
2024-12-11 11:20:18 +0100

    Add Flask-Migrate support

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -15,6 +17,7 @@ def create_app():
     app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static/uploads')
     
     db.init_app(app)
+    migrate.init_app(app, db)
     
     from app import routes
     app.register_blueprint(routes.bp)
