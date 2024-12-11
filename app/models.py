d0d33c6e28dd6059941a947aa327f300582401a9
DimitriGeelen <40521958+DimitriGeelen@users.noreply.github.com>
2024-12-11 11:16:09 +0100

    Update Event model with coordinates

--- app/models.py
+++ app/models.py
@@ -13,6 +13,9 @@ class Event(db.Model):
     postal_code = db.Column(db.String(20))
     file_path = db.Column(db.String(200))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    # Add coordinates for map
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
     
     def to_dict(self):
         return {
@@ -25,5 +28,7 @@ class Event(db.Model):
             'street_name': self.street_name,
             'street_number': self.street_number,
             'postal_code': self.postal_code,
-            'file_path': self.file_path
+            'file_path': self.file_path,
+            'latitude': self.latitude,
+            'longitude': self.longitude
         }
