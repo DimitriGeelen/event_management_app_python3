7258ebedbe0e0651a55fc7751f454fd2f62bcd3f
DimitriGeelen <40521958+DimitriGeelen@users.noreply.github.com>
2024-12-12 21:08:23 +0100

    Fix: Update form with proper validators and file handling

--- app/forms.py
+++ app/forms.py
@@ -1,28 +1,26 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, TextAreaField, DateTimeLocalField, SelectField, SubmitField
+from wtforms import StringField, TextAreaField, DateTimeLocalField, SelectField, FileField
 from wtforms.validators import DataRequired, Optional, Length
+from datetime import datetime
 
 class CategoryForm(FlaskForm):
-    name = StringField('Category Name', validators=[DataRequired(), Length(max=50)])
+    name = StringField('Name', validators=[DataRequired(), Length(max=50)])
     description = TextAreaField('Description', validators=[Optional(), Length(max=200)])
-    submit = SubmitField('Save Category')
 
 class EventForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
+    title = StringField('Title', validators=[DataRequired(), Length(max=100)])
     description = TextAreaField('Description', validators=[Optional()])
-    start_datetime = DateTimeLocalField('Start Date & Time', 
+    start_datetime = DateTimeLocalField('Start Date & Time',
                                       format='%Y-%m-%dT%H:%M',
-                                      validators=[DataRequired()])
-    end_datetime = DateTimeLocalField('End Date & Time', 
+                                      validators=[DataRequired()],
+                                      default=datetime.now)
+    end_datetime = DateTimeLocalField('End Date & Time',
                                     format='%Y-%m-%dT%H:%M',
-                                    validators=[DataRequired()])
-    location_name = StringField('Location Name', validators=[Optional()])
-    street_name = StringField('Street Name', validators=[Optional()])
-    street_number = StringField('Street Number', validators=[Optional()])
-    postal_code = StringField('Postal Code', validators=[Optional()])
-    category_id = SelectField('Category', coerce=int, validators=[Optional()])
-    file = FileField('Upload File', validators=[
-        Optional(),
-        FileAllowed(['jpg', 'jpeg', 'png', 'pdf'], 'Only images and PDF files are allowed!')
-    ])
+                                    validators=[DataRequired()],
+                                    default=datetime.now)
+    location_name = StringField('Location Name', validators=[Optional(), Length(max=100)])
+    street_name = StringField('Street Name', validators=[Optional(), Length(max=100)])
+    street_number = StringField('Street Number', validators=[Optional(), Length(max=20)])
+    postal_code = StringField('Postal Code', validators=[Optional(), Length(max=20)])
+    category_id = SelectField('Category', coerce=int)
+    file = FileField('File (PDF or Image)', validators=[Optional()])
